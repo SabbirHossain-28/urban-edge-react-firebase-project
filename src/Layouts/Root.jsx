d6bb6c55d298b1b2def8247583e89de60997b2de
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-09 09:49:27 +0600

    Create and  Added footer component

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "../Components/Header/Navbar";
+import Footer from "../Components/Footer/Footer";
 const Root = () => {
     return (
         <div className="max-w-7xl mx-auto font-popin">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
