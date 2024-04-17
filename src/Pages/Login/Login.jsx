e80b3964542c6cc1f372b76490034d811c92eec4
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-17 22:49:17 +0600

    Fix some text color issue

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -182,7 +182,7 @@ const Login = () => {
 
                   <div className="flex items-start"></div>
                   <input
-                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    className="w-full text-gray-900 dark:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     type="submit"
                     value="Login"
                   />
